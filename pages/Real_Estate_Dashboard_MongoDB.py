8b355744c6e43e847c0f1b83a15b51f56ac11f02
Trinh Hoang Minh <hoang.minh.trinh@gmail.com>
2025-07-29 15:36:26 +0700

    change name

--- pages/Real_Estate_Dashboard_MongoDB.py
+++ pages/Real_Estate_Dashboard_MongoDB.py
@@ -443,7 +443,7 @@ def main():
                 val_col1, val_col2 = st.columns(2)
                 
                 with val_col1:
-                    st.metric("Enterprise Value (RNAV)", format_vnd_display(enterprise_value))
+                    st.metric("Total Project RNAV", format_vnd_display(enterprise_value))
                     st.metric("Equity Value", format_vnd_display(equity_value))
                 
                 with val_col2:
